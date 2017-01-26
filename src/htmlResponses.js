d48fe63bf9e9588bfed884d4a7f48e28cf38c8c3
Danielle Gray <dg2894@g.rit.edu>
2017-01-26 09:44:22 -0500

    Fix typo

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -4,7 +4,7 @@ const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 
 const getIndex = (request, response) => {
-  response.writeHead(200, { 'Contet-Type': 'text/html' });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
   response.end();
 };
